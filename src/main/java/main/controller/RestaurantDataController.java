539b6691d2eac69ca7189fa661b89a79ab9852be
Ben Thompson <benjamin.philip.thompson@maine.edu>
2021-01-26 23:38:36 -0500

    updated restaurantdata controller + model

--- src/main/java/main/controller/RestaurantDataController.java
+++ src/main/java/main/controller/RestaurantDataController.java
@@ -1,33 +1,67 @@
 package main.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import main.model.Restaurant;
 import main.service.APICallService;
 
 @CrossOrigin
 @RestController
-@RequestMapping(path="/api/v1")
+@RequestMapping(path = "/api/v1")
 public class RestaurantDataController {
 
-	@GetMapping(path="/getRestaurantData")
-	public Restaurant data(
-			@RequestParam(value = "lat", defaultValue = "0") double lat,
+	@GetMapping(path = "/getRestaurantData")
+	public List<Restaurant> data(@RequestParam(value = "lat", defaultValue = "0") double lat,
 			@RequestParam(value = "lng", defaultValue = "0") double lng,
-			@RequestParam(value = "food_type", defaultValue="lunch") String food_type){
-		
-	
-		APICallService api = new APICallService();
-		String dataFromYelp = api.searchForBusinesses(lat,lng,food_type);
+			@RequestParam(value = "food_type", defaultValue = "lunch") String food_type) {
 
-		return new Restaurant(dataFromYelp);
+		List<Restaurant> list = new ArrayList<Restaurant>();
+
+
+		APICallService api = new APICallService();
+		String dataFromYelp = api.searchForBusinesses(lat, lng, food_type);
+		
+		
+		//parsing the json data from yelp
+		JsonObject jsonObject = JsonParser.parseString(dataFromYelp.toString()).getAsJsonObject();
+		JsonArray rest_list = jsonObject.get("businesses").getAsJsonArray();
+		
+		
+		for(int i =0; i < rest_list.size(); i++) {
+			
+			JsonObject restaurant = rest_list.get(i).getAsJsonObject();
+			String name = restaurant.get("name").toString();
+			String id = restaurant.get("id").toString();
+			int review_count = restaurant.get("review_count").getAsInt();
+						
+
+			double rating = restaurant.get("rating").getAsDouble();
+			
+			JsonObject loc = restaurant.get("location").getAsJsonObject();
+			
+			String address = loc.get("address1").toString() + loc.get("address2").toString()
+				+ loc.get("address3").toString() + loc.get("city").toString()
+            + loc.get("state").toString();
+
+
+
+			
+			Restaurant r = new Restaurant(name,id,address,rating,review_count);
+			list.add(r);
+		}
+
+		return list;
 	}
-	
-	
-	
 
 }
--- src/main/java/main/model/Restaurant.java
+++ src/main/java/main/model/Restaurant.java
@@ -2,16 +2,44 @@ package main.model;
 
 public class Restaurant {
 
-	private String restaurant_data;
+	private String name;
+	private String id;
+	private String address;
+	private double rating;
+	private int num_of_reviews;
 	
-	public Restaurant(String restaurant_data) {
-	this.restaurant_data = restaurant_data;	
+	public Restaurant(String name, String id, String address, double rating, int num_of_reviews) {
+		
+	this.name = name;
+	this.id = id;
+	this.address = address;
+	this.rating = rating;
+	this.num_of_reviews = num_of_reviews;
+	
 	}
-	
-	public String getData() {
-		return restaurant_data;
+
+
+	public int getNum_of_reviews() {
+		return num_of_reviews;
 	}
-	
+
+	public double getRating() {
+		return rating;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+
 	
 	
 }
