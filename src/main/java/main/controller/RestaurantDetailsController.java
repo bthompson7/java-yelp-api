f3b54fb7ae8d3cbe25f785ec8ae0f1090bfac602
Ben Thompson <benjamin.philip.thompson@maine.edu>
2021-01-26 22:23:41 -0500

    fix cors issue

--- src/main/java/main/controller/RestaurantDetailsController.java
+++ src/main/java/main/controller/RestaurantDetailsController.java
@@ -1,5 +1,6 @@
 package main.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import main.model.Restaurant;
 import main.service.APICallService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path="/api/v1")
 public class RestaurantDetailsController {
